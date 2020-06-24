ed093d8763c9d13cb9f666e31784b948b10e0f85
roman8301 <roman8301@gmail.com>
2020-06-25 01:13:41 +0300

    Add burger menu

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
-		$('.header__burger,.header__menu').toggleClass('active')
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+		$('').toggleClass('');
+		$('').toggleClass('');
 	});
 });
